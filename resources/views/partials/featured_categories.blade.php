236dc060e20b5af78f7bccc0cf6bf55db4914570
Adam Sabbah <Adam Sabbah>
2023-10-15 16:18:40 +0300

    updates

--- resources/views/partials/featured_categories.blade.php
+++ resources/views/partials/featured_categories.blade.php
@@ -5,7 +5,7 @@
             @foreach ($FeateredCategories as $category)
                 <div class="col-md-3 to-animate">
                     <div class="fh5co-type text-center">
-                        <img src="{{ url($category->image) }}" alt="{{ $category->name }}" style="width: 100%">
+                        <img src="{{ url($category->image) }}" alt="{{ $category->name }}">
                         <h3 style="padding-top:0">{{ $category->name }}</h3>
                         <p>{{ $category->description }}</p>
                     </div>
