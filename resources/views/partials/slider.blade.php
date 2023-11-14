6a5c89fddb75f87e3cdadbefde925f71f98b2b98
Adam Sabbah <Adam Sabbah>
2023-11-14 10:14:30 +0300

    updates

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,14 +1,55 @@
 
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="{{ url($MainImage1->image) }}" class="d-block w-100">
-        </div>
-        <div class="carousel-item">
-            <img src="{{ url($MainImage2->image) }}" class="d-block w-100">
-        </div>
-        <div class="carousel-item">
-            <img src="{{ url($MainImage3->image) }}" class="d-block w-100">
-        </div>
+        @if ($MainImage1->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage1->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage2->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage2->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage3->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage3->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage4->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage4->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage5->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage5->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage6->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage6->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage7->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage7->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage8->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage8->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage9->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage9->image) }}" class="d-block w-100">
+            </div>
+        @endif
+        @if ($MainImage10->image)
+            <div class="carousel-item active">
+                <img src="{{ url($MainImage10->image) }}" class="d-block w-100">
+            </div>
+        @endif
     </div>
 </div>
